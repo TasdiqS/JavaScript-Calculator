cd6c90d55e0f506bb8f0b808d9c76366dd643a11
TasdiqS <tasdiqspace@gmail.com>
2022-02-14 14:31:47 +0530

    message

---
+++ index.js
@@ -0,0 +1,36 @@
+let screen=document.getElementById('screen');
+buttons=document.querySelectorAll('button');
+let screenValue='';
+for(item of buttons){
+    item.addEventListener('click',(e)=>{
+        buttonText=e.target.innerText;
+        console.log('Button text iss ',buttonText)
+
+        if(buttonText=='X'){
+            buttonText='*';
+            screenValue +=buttonText;
+            screen.value =screenValue;
+
+        }
+        else if(buttonText=='C'){
+            screenValue ="";
+            screen.value =screenValue;
+
+        }
+        else if(buttonText=='='){
+            screen.value=eval(screenValue);
+            
+        }
+        else if(buttonText == 'del'){
+            backspace = screen.value;
+            restNum = backspace.slice(0,-1);
+            screenValue = restNum;
+            screen.value = screenValue;
+        }
+        else{
+            screenValue +=buttonText;
+            screen.value =screenValue;
+        }
+
+    })
+}
